2cac0efebc9e7db7cb990cd28b1d4d99f3b467a1
NikitaPeshko <n.peshko@mail.ru>
2021-12-05 12:09:17 +0300

    add test by need fix

--- Second/module2/src/main/test/java/com/epam/esc/servise/GiftServiceImpTest.java
+++ Second/module2/src/main/test/java/com/epam/esc/servise/GiftServiceImpTest.java
@@ -2,12 +2,12 @@ package com.epam.esc.servise;
 
 import com.epam.esc.DTO.GiftDTO;
 import com.epam.esc.dao.GiftDAOImp;
+import com.epam.esc.exception.DaoException;
 import com.epam.esc.exception.NoEntityException;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -57,7 +57,29 @@ class GiftServiceImpTest {
     }
 
     @Test
-    void getGiftById() {
+    void getGiftById() throws DaoException {
+        GiftDTO model = new GiftDTO();
+
+        model.setId(1);
+        model.setDiscription("spa salon minsk nikita 123");
+        model.setPrice(10);
+        model.setDuration(7);
+        model.setCreateDate("1970-01-19T23:06Z");
+        model.setLastUpdateDate("1970-01-19T23:06Z");
+        Set<String> tags=new HashSet<>();
+        tags.add("spa");
+        tags.add("minsk");
+        tags.add("sport");
+        model.setTags(tags);
+
+        when(repositoryMock.getGiftById(1)).thenReturn(model);
+
+        GiftDTO actual = service.getGiftById(1);
+
+        verify(repositoryMock, times(1)).getGiftById(1);
+        verifyNoMoreInteractions(repositoryMock);
+
+        assertThat(actual, is(model));
     }
 
     @Test
