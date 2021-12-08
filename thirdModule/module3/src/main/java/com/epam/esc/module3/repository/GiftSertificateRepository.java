74f5b309a9cfe340141acc20faa81fe46b85faf5
NikitaPeshko <n.peshko@mail.ru>
2021-12-08 18:54:06 +0300

    add giftrepo

---
+++ thirdModule/module3/src/main/java/com/epam/esc/module3/repository/GiftSertificateRepository.java
@@ -0,0 +1,10 @@
+package com.epam.esc.module3.repository;
+
+
+import com.epam.esc.module3.entity.Gift;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GiftSertificateRepository extends JpaRepository<Gift, Integer> {
+}
